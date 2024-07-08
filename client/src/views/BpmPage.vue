28b1671f91023277a9c401cc0ef9debb9d630396
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-07-08 13:44:50 +0800

    Added views

---
+++ client/src/views/BpmPage.vue
@@ -0,0 +1,13 @@
+<!-- src/views/BPM.vue -->
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-white text-3xl font-bold mb-4">BPM</h1>
+    <p class="text-white text-lg text-center">BPM Page</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+/* Add any scoped styles specific to this page if needed */
+</style>
---
+++ client/src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<!-- src/views/Home.vue -->
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-white text-3xl font-bold mb-4">Home</h1>
+    <p class="text-white text-lg text-center">Home Page</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+/* Add any scoped styles specific to this page if needed */
+</style>
