63406aa0b09c8cbe74435fbcab8803f75b25da1a
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-07-30 15:09:31 +0800

    Fix CORS settings

--- heartBPMusic/settings.py
+++ heartBPMusic/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,12 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
-    "https://heart-bpm-music.vercel.app/",
+    "https://heart-bpm-music.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
