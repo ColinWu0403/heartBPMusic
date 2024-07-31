fc5b187e2cacb696c055510a531cef967a365eb7
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-07-31 14:19:14 +0800

    Temp fix

--- heartBPMusic/settings.py
+++ heartBPMusic/settings.py
@@ -56,10 +56,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "https://heart-bpm-music.vercel.app",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8000",
+#     "https://heart-bpm-music.vercel.app",
+#     "https://heartbpmusic.onrender.com",
+# ]
 
 CORS_ALLOW_CREDENTIALS = True
 
