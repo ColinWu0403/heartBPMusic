3099f611f814714b15da61e2dbe86cd75a753906
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-07-31 15:35:17 +0800

    Update settings.py

--- heartBPMusic/settings.py
+++ heartBPMusic/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = ['*']
