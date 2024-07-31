c952b199b8a04e0e37cf58833e3174e6fde43a58
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-07-31 14:54:55 +0800

    Session cookie 403 fix

--- heartBPMusic/settings.py
+++ heartBPMusic/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'heartBPMusic.middleware.SessionCookieSameSiteWorkaround',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
